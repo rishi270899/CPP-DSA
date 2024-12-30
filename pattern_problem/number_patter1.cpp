5ad0e93bd3b2bec33c9928c76985490fbf3b2efe
Rishi Goyal <rishig516@gmail.com>
2024-12-30 22:29:44 +0530

    char patter

--- pattern_problem/number_patter1.cpp
+++ pattern_problem/number_patter1.cpp
@@ -1,19 +1,41 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int n = 5;
+//     int num = 1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         for (int j = 1; j <= n; j++)
+//         {
+//             cout << num << " ";
+//             num++;
+//         }
+
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// we can achive this result without using second variable
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
     int n = 5;
-    int num = 1;
 
     for (int i = 1; i <= n; i++)
     {
-
         for (int j = 1; j <= n; j++)
         {
-            cout << num << " ";
-            num++;
+            cout << (i - 1) * n + j << " ";
         }
 
         cout << endl;
